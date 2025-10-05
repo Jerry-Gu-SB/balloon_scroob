6d59e8457c94c67c9b6eb9cc6a5ec8769de7e80a
Jerry Gu <ncq9fn@virginia.edu>
2025-10-05 19:26:44 -0400

    Added impulse to physics handler and refactored a bit

--- Player/Scripts/player_physics_handler.gd
+++ Player/Scripts/player_physics_handler.gd
@@ -8,8 +8,14 @@ const INPUT_MULTIPLYER = 2.5
 const MAX_SPEED = 250
 
 func update(input_vector : Vector2, character_body : CharacterBody2D) -> void:
+	# apply_impulse()
 	resolve_input(input_vector)
-	resolve_physics(character_body)
+	resolve_environmental_forces()
+	apply_physics(character_body)
+	return
+
+func apply_impulse(impulse_vector : Vector2) -> void:
+	velocity += impulse_vector
 	return
 
 func resolve_input(input_vector : Vector2) -> void:
@@ -18,12 +24,12 @@ func resolve_input(input_vector : Vector2) -> void:
 	if abs(velocity[1]) < MAX_SPEED:
 		velocity[1] += input_vector[1] * INPUT_MULTIPLYER
 	
-func resolve_physics(character_body : CharacterBody2D) -> void:
+func resolve_environmental_forces() -> void:
 	velocity += gravity
-	
 	velocity[0] = move_towards_zero(velocity[0], AIR_DRAG[0])
 	velocity[1] = move_towards_zero(velocity[1], AIR_DRAG[1])
 	
+func apply_physics(character_body : CharacterBody2D) -> void:
 	character_body.velocity = velocity
 	character_body.move_and_slide()
 	
