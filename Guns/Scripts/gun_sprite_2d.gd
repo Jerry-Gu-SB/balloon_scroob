d9f34f8933a9012f219eb19021cc2448103bf549
Jerry Gu <ncq9fn@virginia.edu>
2025-10-16 22:43:32 -0400

    Added player sprite and added flipping logic for guns

--- Guns/Scripts/gun_sprite_2d.gd
+++ Guns/Scripts/gun_sprite_2d.gd
@@ -3,3 +3,7 @@ extends Sprite2D
 func _ready() -> void:
 	var parent : GunProvider = get_parent()
 	self.texture = parent.get_gun_data().sprite_texture
+	
+func _process(_delta : float) -> void:
+	var mouse_position = get_global_mouse_position()
+	self.flip_v = mouse_position[0] < self.global_position[0]
