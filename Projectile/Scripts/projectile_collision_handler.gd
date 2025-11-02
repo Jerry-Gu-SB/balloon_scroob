f58d4f4428b964f6292846bfbf003b25ff6c93b3
Jerry Gu <ncq9fn@virginia.edu>
2025-11-01 21:45:51 -0400

    Expanded map, added tile health

--- Projectile/Scripts/projectile_collision_handler.gd
+++ Projectile/Scripts/projectile_collision_handler.gd
@@ -1,12 +1,16 @@
 extends Node
 
-@onready var area2D : Area2D = $"../Area2D"
+@onready var hurtbox : Area2D = $"../Hurtbox"
+@onready var explosion_area : Area2D = $"../Explosion Area"
 
 func _ready() -> void:
-	area2D.body_entered.connect(on_body_entered)
+	hurtbox.body_entered.connect(on_body_entered)
 
 func on_body_entered(collision) -> void:
 	if collision.has_method("take_damage"):
 		var parent : ProjectileProvider = get_parent()
 		collision.take_damage(parent.get_projectile_data().damage)
 		queue_free()
+		
+func get_all_collisions() -> void:
+	explosion_area.get_overlapping_areas()
