5d8f202f079faf7a13b73ae332498982c8ce4e2f
Jerry Gu <ncq9fn@virginia.edu>
2025-11-03 20:55:04 -0500

    Added pitch scaling for the gun sfx and also added map tile collision for the wooden boards

--- Guns/Pistol/gun_audio_stream_players.gd
+++ Guns/Pistol/gun_audio_stream_players.gd
@@ -2,8 +2,11 @@ extends Node
 
 @onready var gunshot_sfx : AudioStreamPlayer = $GunShot
 
+@export var pitch_scale_strength : float = .05
+
 func _ready() -> void:
 	EventBus.fire_gun.connect(process_fire_gun)
 	
 func process_fire_gun() -> void:
+	gunshot_sfx.pitch_scale = randf_range(1 - pitch_scale_strength, 1 + pitch_scale_strength)
 	gunshot_sfx.play()
